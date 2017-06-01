0ed49b407cf9d7e0497138617df899cc048b9b78
Sai Talluri <saitalluri@MacBook-Air.local>
2017-06-01 13:30:30 -0400

    bug fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 var app = express();
 
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
 
 app.use(express.static(__dirname + '/public'));
 
